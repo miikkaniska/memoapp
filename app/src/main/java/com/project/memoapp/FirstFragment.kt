5b954fc12643c3a9bd85a24dc14e41bd1aec701b
Tomi Kääriäinen <tomikaariainen63@gmail.com>
2023-11-16 16:02:30 +0200

    FirstFragment toiminnallisuutta ja MyAdapter muutosta

--- app/src/main/java/com/project/memoapp/FirstFragment.kt
+++ app/src/main/java/com/project/memoapp/FirstFragment.kt
@@ -1,17 +1,16 @@
 package com.project.memoapp
 
+import MyAdapter
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.firestore.FirebaseFirestore
 import com.project.memoapp.databinding.FragmentFirstBinding
-
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -19,71 +18,61 @@ import com.project.memoapp.databinding.FragmentFirstBinding
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
-    private val binding get() = _binding!!
 
-    private lateinit var db: FirebaseFirestore
+    private val binding get() = _binding!!
+    private val memoList = mutableListOf<MemoData>()
+    private val db = Firebase.firestore
+
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: MyAdapter
-    // This property is only valid between onCreateView and
-    // onDestroyView.
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
+    ): View {
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        db = FirebaseFirestore.getInstance()
         recyclerView = binding.recyclerView
-        val data: List<String> = mutableListOf()
-        adapter = MyAdapter(data)
-
+        adapter = MyAdapter(memoList) // Muutetaan adapteri käyttämään Firestoresta haettua listaa
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = adapter
 
-
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
-        fetchDataFromFirestore()
+        fetchDataFromFirestore() // Kutsutaan funktiota, joka hakee tiedot Firestoresta
     }
 
     private fun fetchDataFromFirestore() {
         db.collection("memos")
             .get()
             .addOnSuccessListener { result ->
-                val data: MutableList<String> = mutableListOf()
+                val data: MutableList<MemoData> = mutableListOf()
 
                 for (document in result) {
                     // Käsittele dokumentti ja hae halutut tiedot
-                    val formattedTime = document.getLong("creationTime")
-                    val email = document.getString("email")
-                    val nameOfMemo = document.getString("nameOfMemo")
-                    val nickname = document.getString("nickname")
-                    val shared = document.getBoolean("shared")
+                    val email = document.getString("email") ?: ""
+                    val nameOfMemo = document.getString("nameOfMemo") ?: ""
+                    val nickname = document.getString("nickname") ?: ""
+                    val isShared = document.getBoolean("isShared") ?: false
 
-                    // Luo merkkijono tiedoista ja lisää se datalistaan
-                    val itemString = "Creation Time: $formattedTime\nEmail: $email\nMemo: $nameOfMemo\nName: $nickname\nShared: $shared"
-                    data.add(itemString)
+                    val creationTime = if (document.contains("creationTime")) {
+                        document.getLong("creationTime") ?: System.currentTimeMillis()
+                    } else {
+                        System.currentTimeMillis() // Tai anna oletusaika, jos creationTime-kenttä ei ole saatavilla
+                    }
+                    val memo = MemoData(nameOfMemo, nickname, email, isShared, creationTime)
+                    data.add(memo)
                 }
-
                 // Päivitä RecyclerView adapterin avulla
                 adapter = MyAdapter(data)
                 recyclerView.adapter = adapter
             }
-            .addOnFailureListener { exception ->
-                // Käsittely virhetilanteessa
-                Log.e("Firestore", "Error getting documents: ", exception)
-            }
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
